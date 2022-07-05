d1c844229bdde7374fd35ebde741dd3cebc04277
mgsrevolver <clayreimus@gmail.com>
2022-07-05 17:07:14 -0400

    full list working

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types'
 
 import './App.css'
 
+const APIkey = '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'
+const allModsURL =
+  'https://api.github.com/repos/Roll20/roll20-api-scripts/contents/'
+
 const ModsType = PropTypes.shape({
   id: PropTypes.string.isRequired,
   name: PropTypes.shape({
@@ -59,14 +63,40 @@ ModsInfo.propTypes = ModsType
 function App() {
   const [mods, setMods] = React.useState([])
   const [selectedMods, selectedModsSet] = React.useState(null)
-  const APIurl =
-    'https://api.github.com/repos/Roll20/roll20-api-scripts/contents/'
 
   React.useEffect(() => {
-    fetch(APIurl)
+    fetch(allModsURL, {
+      method: 'GET',
+      headers: {
+        Authorization: APIkey,
+      },
+    })
       .then((res) => res.json())
       .then((data) => setMods(data))
   }, [])
+
+  const getMoreInfo = ({ mods }) => {
+    this.setState(
+      {
+        modName: mods.name,
+      },
+      () => {
+        fetch(
+          `https://api.github.com/repositories/28607958/contents/${this.state.modName}/README.md`,
+          {
+            method: 'GET',
+            headers: {
+              Authorization: APIkey,
+            },
+          }
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data)
+          })
+      }
+    )
+  }
 
   return (
     <div
@@ -76,7 +106,7 @@ function App() {
         paddingTop: '1em',
       }}
     >
-      <h1 className="title">Roll20 Mods Search</h1>
+      <h1 className="title">Roll20 Mods Directory</h1>
       <div
         style={{
           display: 'grid',
@@ -90,7 +120,10 @@ function App() {
               {mods.map((mods) => (
                 <ModsRow
                   mods={mods}
-                  onClick={(mods) => selectedModsSet(mods)}
+                  onClick={(mods) => {
+                    selectedModsSet(mods)
+                    getMoreInfo(mods)
+                  }}
                 />
               ))}
             </tbody>
