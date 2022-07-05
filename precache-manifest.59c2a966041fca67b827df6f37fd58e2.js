63a14a4a002ffe8f1ece826662f4d34ba672a989
mgsrevolver <clayreimus@gmail.com>
2022-07-05 19:00:34 -0400

    Updates

---
+++ precache-manifest.59c2a966041fca67b827df6f37fd58e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98fdab2e50d318f205e6968a29587712",
+    "url": "/Roll20-API-Directory/index.html"
+  },
+  {
+    "revision": "e9a61dc9c2aec8250979",
+    "url": "/Roll20-API-Directory/static/css/main.063073f9.chunk.css"
+  },
+  {
+    "revision": "7e368abdf59b7a056513",
+    "url": "/Roll20-API-Directory/static/js/2.045960d1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Roll20-API-Directory/static/js/2.045960d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9a61dc9c2aec8250979",
+    "url": "/Roll20-API-Directory/static/js/main.a0e4d481.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Roll20-API-Directory/static/js/main.a0e4d481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5df24d29d85f3bcfeb5",
+    "url": "/Roll20-API-Directory/static/js/runtime-main.430309bc.js"
+  }
+]);
